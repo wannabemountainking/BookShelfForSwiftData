238487f7886f024299f824ac77e4b218f4b721db
wannabemountainking <kdy0096@gmail.com>
2026-03-15 17:09:43 +0900

    Initial Commit

---
+++ BookShelfForSwiftData/BookShelfForSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfForSwiftDataApp.swift
+//  BookShelfForSwiftData
+//
+//  Created by YoonieMac on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfForSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookShelfForSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookShelfForSwiftData
+//
+//  Created by YoonieMac on 3/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
